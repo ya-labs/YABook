5ac32cb1c2b2933d78e3a81114951dc6d70fac3d
nicolasmacardoso <nicolasxzx1225@gmail.com>
2026-07-20 08:53:27 -0300

    feat: cria contrato inicial do núcleo Tauri

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,7 +1,25 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct AppStatus {
+    name: String,
+    version: String,
+}
+
+#[tauri::command]
+fn get_app_status() -> AppStatus {
+    AppStatus {
+        name: "YABook Desktop".into(),
+        version: env!("CARGO_PKG_VERSION").into(),
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![get_app_status])
         .run(tauri::generate_context!())
         .expect("error while running YABook Desktop");
 }
